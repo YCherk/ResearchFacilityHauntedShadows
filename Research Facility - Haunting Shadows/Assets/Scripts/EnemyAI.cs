08cc57baef4fc2f4aaae2c6014cc65f9d729ef93
YCherk <117645287+YCherk@users.noreply.github.com>
2023-12-25 19:07:18 +0000

    Audio
    I updated the enemy AI script to play audio based on the state the enemy is in. I also added an event at the start of the attack animation which plays the audio for Attack.

--- Research Facility - Haunting Shadows/Assets/Scripts/EnemyAI.cs
+++ Research Facility - Haunting Shadows/Assets/Scripts/EnemyAI.cs
@@ -6,14 +6,18 @@ public class EnemyAI : MonoBehaviour
     public float patrolRadius = 10.0f;
     public float patrolTimer = 5.0f;
     public float moveSpeed = 2.0f;
-    public float chaseSpeed = 4.0f; // Speed when chasing the player
+    public float chaseSpeed = 4.0f;
     public Transform player;
     public float attackDistance = 2.0f;
     public float sightDistance = 15.0f;
     public float backSightDistance = 5.0f;
     public float fieldOfView = 60.0f;
     public float backFieldOfView = 90.0f;
-    public AudioSource walkingAudio;
+
+    // Audio sources for different states
+    public AudioSource walkingAudioSource;
+    public AudioSource runningAudioSource;
+    public AudioSource attackAudioSource;
 
     private float timer;
     private NavMeshAgent agent;
@@ -86,11 +90,59 @@ public class EnemyAI : MonoBehaviour
         }
 
         animator.SetBool("isIdle", agent.velocity.magnitude < 0.01f);
-
         if (agent.velocity.magnitude > 0.01f && !canSeePlayerFromBehind)
         {
             transform.rotation = Quaternion.LookRotation(agent.velocity.normalized);
         }
+
+        // Handle audio based on the enemy state
+        HandleAudioPlayback();
+    }
+
+    void HandleAudioPlayback()
+    {
+        // Walking audio
+        if (IsWalking() && !walkingAudioSource.isPlaying)
+        {
+            walkingAudioSource.Play();
+            runningAudioSource.Stop();
+            attackAudioSource.Stop();
+        }
+        // Running audio
+        else if (isChasingPlayer && !runningAudioSource.isPlaying)
+        {
+            runningAudioSource.Play();
+            walkingAudioSource.Stop();
+            attackAudioSource.Stop();
+        }
+        
+        
+        // Stop audio if idle
+        else if (IsIdle())
+        {
+            walkingAudioSource.Stop();
+            runningAudioSource.Stop();
+            attackAudioSource.Stop();
+        }
+    }
+
+    bool IsWalking()
+    {
+        return !isChasingPlayer && !isSearchingForPlayer && agent.velocity.magnitude > 0.01f;
+    }
+
+    public void PlayAttackSound()
+    {
+        if (attackAudioSource != null && !attackAudioSource.isPlaying)
+        {
+            attackAudioSource.Play();
+        }
+    }
+
+
+    bool IsIdle()
+    {
+        return agent.velocity.magnitude < 0.01f;
     }
 
     public void AttackPlayer()
@@ -148,8 +200,10 @@ public class EnemyAI : MonoBehaviour
             timer = 0;
         }
 
-        animator.SetBool("WalkForward", agent.velocity.magnitude > 0.01f);
-        walkingAudio.Play();
+        if (agent.velocity.magnitude > 0.01f && !walkingAudioSource.isPlaying)
+        {
+            walkingAudioSource.Play();
+        }
     }
 
     public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
