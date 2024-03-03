da024db8f9ab41207647ecef91ff638bc5325bd5
YCherk <117645287+YCherk@users.noreply.github.com>
2024-03-03 18:20:21 +0000

    Update DifficultySettingHandler.cs

--- Research Facility - Haunting Shadows/Assets/Scripts/DifficultySettingHandler.cs
+++ Research Facility - Haunting Shadows/Assets/Scripts/DifficultySettingHandler.cs
@@ -1,19 +1,19 @@
 using UnityEngine;
-using TMPro; // Import the TextMeshPro namespace
+using TMPro; 
 
 public class DifficultySettingHandler : MonoBehaviour
 {
-    public TMP_Dropdown difficultyDropdown; // Change to TMP_Dropdown type
+    public TMP_Dropdown difficultyDropdown; 
 
     void Start()
     {
-        // Load the previously selected difficulty, defaulting to the first option if none was previously selected
+        // Load the previoously selected difficulty, defaulting to the first option if none was previously selected
         difficultyDropdown.value = PlayerPrefs.GetInt("SelectedDifficulty", 0);
     }
 
     public void OnDifficultyChanged()
     {
-        // Save the selected difficulty
+        // saves th selected difficulty
         PlayerPrefs.SetInt("SelectedDifficulty", difficultyDropdown.value);
         PlayerPrefs.Save();
     }
